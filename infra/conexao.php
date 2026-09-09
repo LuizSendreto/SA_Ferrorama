0b8e03e3b661dbecff7eb58e4c57f6d6035118f3
Rafael-Thais <rafael_tironi1@estudante.sesisenai.org.br>
2026-09-09 08:00:16 -0300

    add: fiz a conexao com o banco de dados

---
+++ infra/conexao.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "";
+
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if($conn-> connect_error){
+   die ("Erro na conexão:" . $conn-> connect_error);
+};
+
+?>
